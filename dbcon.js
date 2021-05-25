da4d099cc37561253ebe3de27360d4412b7f44cf
Michael Arras <59668958+mikearras@users.noreply.github.com>
2021-05-24 23:37:49 -0700

    Delete dbcon.js

--- dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_bonacib',
-  password        : '0816',
-  database        : 'cs340_bonacib'
-});
-module.exports.pool = pool;
