78f30093a4971ae40959475e47d279beece26e8b
Mike Arras <arrasm@oregonstate.edu>
2021-06-04 23:22:21 -0700

    orderCatalogItems broken but better

--- orderCatalogItem.js
+++ orderCatalogItem.js
@@ -2,20 +2,9 @@ module.exports = function () {
     var express = require('express');
     var router = express.Router();
 
-    function getMembers(res, mysql, context, complete) {
-        mysql.pool.query("SELECT memberID, firstName, lastName FROM Member", function (error, results, fields) {
-            if (error) {
-                res.write(JSON.stringify(error));
-                res.end();
-            }
-            context.members = results;
-            complete();
-        });
-    }
-
 
     function getOrders(res, mysql, context, complete) {
-        mysql.pool.query("SELECT orderDate, orderNumber, memberID FROM Orders", function (error, results, fields) {
+        mysql.pool.query("SELECT orderNumber FROM Orders", function (error, results, fields) {
             if (error) {
                 res.write(JSON.stringify(error));
                 res.end();
@@ -25,18 +14,33 @@ module.exports = function () {
         });
     }
 
-    function getMemberOrders(res, mysql, context, complete) {
+    function getCatalogItems(res, mysql, context, complete) {
+        mysql.pool.query("SELECT catalogID FROM CatalogItems", function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.catalogItem = results;
+            complete();
+        });
+
+    }
+
+
+    function getOrderCatalogItems(res, mysql, context, complete) {
         mysql.pool.query("SELECT catalogID, orderNumber FROM OrderCatalogItems", function (error, results, fields) {
             if (error) {
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            context.memberOrders = results;
+            context.orderCatalogItem = results;
             complete();
         });
+
     }
 
-    // Display all authorItems
+
+    // Display all OrderCatalogItems
 
     router.get('/', function (req, res) {
         var callbackCount = 0;
@@ -44,24 +48,31 @@ module.exports = function () {
         // context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
         context.css = ['styles.css'];
         var mysql = req.app.get('mysql');
-        getMembers(res, mysql, context, complete);
+
         getOrders(res, mysql, context, complete);
-        getMemberOrders(res, mysql, context, complete);
+        getCatalogItems(res, mysql, context, complete);
+        getOrderCatalogItems(res, mysql, context, complete);
+
         function complete() {
             callbackCount++;
             if (callbackCount >= 3) {
+                console.log(context.catalog);
                 res.render('orderCatalogItem', context);
             }
         }
+
     });
 
-    /* Adds an order*/
+
+
+
+    /* Adds an orderCatalogItem*/
 
     router.post('/', function (req, res) {
         var mysql = req.app.get('mysql');
-        var sql = "INSERT INTO OrderCatalogItems (catalogID, orderNumber) VALUES (?,?)";
-        // these insert values should match the variables in authorRecord.handlebars
-        var inserts = [req.body.catalogID, req.body.orderNumber];
+        var sql = "INSERT INTO OrderCatalogItems (orderNumber, catalogID) VALUES (?,?)";
+        var inserts = [req.body.orderNumber, req.body.catalogID];
+        console.log(inserts);
         sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
             if (error) {
                 console.log(JSON.stringify(error))
