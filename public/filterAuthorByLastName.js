adeebbac6db9f3d7dce34481361063587db8dd48
Mike Arras <arrasm@oregonstate.edu>
2021-06-06 17:37:29 -0700

    minor style changes, fixed search

--- public/filterAuthorByLastName.js
+++
@@ -1,7 +0,0 @@
-function filterAuthorByLastName() {
-    //get the id of the selected homeworld from the filter dropdown
-    var author_id = document.getElementById('lastName_filter').value
-    //construct the URL and redirect to it
-    //need changes
-    window.location = '/authorRecords/filter/' + parseInt(author_id)
-}
--- public/searchTitle.js
+++
@@ -1,8 +0,0 @@
-console.log("searchTitle.js is refrenced")
-function searchItemByTitle() {
-    console.log("inside the searchItemByTitle")
-    //get the title
-    var title_search_string  = document.getElementById('title_search_string').value
-    //construct the URL and redirect to it
-    window.location = '/item/search/' + encodeURI(title_search_string)
-}
